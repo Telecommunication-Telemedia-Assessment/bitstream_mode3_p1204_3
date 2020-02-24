added fix to per-sec score calculation bug

p1204_3/modelutils.py
@@ -112,7 +112,11 @@ def per_sample_interval_function(mos_O27, prediction_features, intervall=1):
     for i in range(0, int(prediction_features["duration"].values[0])):
         mean_qp_non_i = float(prediction_features["QPValuesStatsPerGop_mean_Av_QPBB_non-i"])
         mean_qp_per_sec = float(prediction_features["QPstatspersecond_mean_qpbb_non_i_" + str(i) + "_sec"])
-        mos_per_sec = (mean_qp_non_i / mean_qp_per_sec) * mos_O27
+        if mean_qp_per_sec != 0:
+            mos_per_sec = (mean_qp_non_i / mean_qp_per_sec) * mos_O27
+        else:
+            mos_per_sec = mos_O27
+        mos_per_sec = np.clip(mos_per_sec,1,5)
         mos_O22.append(mos_per_sec)
     return mos_O22
 