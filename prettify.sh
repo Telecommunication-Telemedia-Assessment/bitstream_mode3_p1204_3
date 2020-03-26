6d20d7e7d7eddcd839dc52f41762905c2f036ff9
Steve <stg7@gmx.de>
2020-03-26 18:01:00 +0100

    prettify

--- prettify.sh
+++ prettify.sh
@@ -3,3 +3,6 @@
 find p1204_3 -name *.py \
     | grep -v "bitstream_mode3_videoparser" \
     | grep -v "__pycache__" | xargs -i black -l 120 {}
+
+find tests -name *.py \
+    | grep -v "__pycache__" | xargs -i black -l 120 {}
--- tests/test_p1204_3.py
+++ tests/test_p1204_3.py
@@ -5,30 +5,15 @@ from p1204_3.utils import json_load
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
 
 
 def test_model_prediction_h264():
-    root_dir = os.path.join(
-        os.path.dirname(__file__),
-        ".."
-    )
-    test_videos_dir = os.path.join(
-        root_dir,
-        "test_videos"
-    )
-    test_video = os.path.join(
-        test_videos_dir,
-        "test_video_h264.mkv"
-    )
+    root_dir = os.path.join(os.path.dirname(__file__), "..")
+    test_videos_dir = os.path.join(root_dir, "test_videos")
+    test_video = os.path.join(test_videos_dir, "test_video_h264.mkv")
 
-    model_config_filename = os.path.join(
-        root_dir,
-        "p1204_3",
-        "models",
-        "p1204_3",
-        "config.json"
-    )
+    model_config_filename = os.path.join(root_dir, "p1204_3", "models", "p1204_3", "config.json")
 
     res = predict_quality(
         test_video,
@@ -37,19 +22,11 @@ def test_model_prediction_h264():
         device_resolution="3840x2160",
         viewing_distance="1.5xH",
         display_size=55,
-        temporary_folder=os.path.join(
-            test_videos_dir,
-            "parsed"
-        )
+        temporary_folder=os.path.join(test_videos_dir, "parsed"),
     )
 
-    ref_report = os.path.join(
-        test_videos_dir,
-        "reports",
-        "test_video_h264.json"
-    )
+    ref_report = os.path.join(test_videos_dir, "reports", "test_video_h264.json")
     ref_res = json_load(ref_report)
     for k in set(list(res.keys()) + list(ref_res.keys())) - set(["date", "video_full_path"]):
-        assert(k in ref_res and k in res)
-        assert(ref_res[k] == res[k])
-
+        assert k in ref_res and k in res
+        assert ref_res[k] == res[k]
