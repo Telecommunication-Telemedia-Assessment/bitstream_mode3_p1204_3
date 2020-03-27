a8f39699f121d46b76950521654590c8d27431a7
Steve <stg7@gmx.de>
2020-03-28 00:40:37 +0100

    fix build script for video parser, it is now build.sh minimal other changes regarding pathes

--- p1204_3/videoparser.py
+++ p1204_3/videoparser.py
@@ -3,27 +3,39 @@ import os
 import sys
 import logging
 
-from p1204_3.generic import *
+from p1204_3.generic import VIDEOPARSER_REPO
 from p1204_3.utils import file_open
 
 
-def run_videoparser(video_seqment_file, output_dir_full_path, skipexisting=True):
+def __video_parser_dir():
+    this_path = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(
+        this_path,
+        "bitstream_mode3_videoparser",
+    )
+
+
+def run_videoparser(video_segment_file, output_dir_full_path, skipexisting=True):
     """
     Run video parser on a video, save report to output_dir_full_path
     """
-    logging.info("run bitstream parser for {}".format(video_seqment_file))
-    report_file_name = (
-        output_dir_full_path + "/" + os.path.splitext(os.path.basename(video_seqment_file))[0] + ".json.bz2"
+    logging.info("run bitstream parser for {}".format(video_segment_file))
+    report_file_name = os.path.join(
+        output_dir_full_path,
+        os.path.splitext(os.path.basename(video_segment_file))[0] + ".json.bz2"
     )
     if skipexisting and os.path.isfile(report_file_name):
         return report_file_name
-    this_path = os.path.dirname(os.path.realpath(__file__))
-    cmd = this_path + """/bitstream_mode3_videoparser/parser.sh "{video}" --output "{report}" """.format(
-        video=video_seqment_file, report=report_file_name
+
+    parser_script = os.path.join(
+        __video_parser_dir(),
+        "parser.sh"
     )
+
+    cmd = f"""{parser_script} "{video_segment_file}" --output "{report_file_name}" """
     ret = os.system(cmd)
     if ret != 0:
-        logging.error(f"there was something wrong with {video_seqment_file}")
+        logging.error(f"there was something wrong with {video_segment_file}")
         logging.error(f"please check the following command: \n {cmd}")
         return ""
     return report_file_name
@@ -34,10 +46,11 @@ def check_or_install_videoparser():
     Check if videoparser is installed, otherwise install it
     """
     logging.info("check or install video parser")
-    this_path = os.path.dirname(os.path.realpath(__file__))
-    videoparser_directory = f"{this_path}/bitstream_mode3_videoparser"
+    videoparser_directory = __video_parser_dir()
+
     if os.path.isdir(videoparser_directory):
         logging.info("video parser is checked out")
         return
     os.system(f"git clone {VIDEOPARSER_REPO} {videoparser_directory}")
-    os.system(f"{videoparser_directory}/build_and_test.sh")
+    os.system(os.path.join(videoparser_directory, "build.sh"))
+
